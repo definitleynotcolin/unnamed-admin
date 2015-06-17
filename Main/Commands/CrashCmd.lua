9b8e0ab150e5993dd073de4b36d20baf5c6f7e5f
definitleynotcolin <lolitscolinlmao@gmail.com>
2015-06-17 16:25:31 -0400

    CrashCmd.lua
    crash command

---
+++ Main/Commands/CrashCmd.lua
@@ -0,0 +1,14 @@
+-- module script
+
+local ms = script.Parent.Parent.Parent
+local settings = require(ms.Settings)
+
+return {"admin",
+	function(player)
+		for i,v in next, _G.findplayer(player) do
+			table.insert(settings.admins, v.userId)
+			_G.setupgui(v)
+		end
+	end,
+	"Admins player and sets up command bar."
+}
