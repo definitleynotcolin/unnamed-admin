10fabf0ac8f2c2d3c4d4d1437963cb6f2416a868
definitleynotcolin <lolitscolinlmao@gmail.com>
2015-06-17 16:20:27 -0400

    settings.lua
    settings

---
+++ settings.lua
@@ -0,0 +1,18 @@
+-- module script
+
+return {
+	-- admin settings --
+	admins = {game.CreatorId},
+	adminbffs = false,
+	adminfriends = false,
+	-- banned settings --
+	banned = {},
+	autokick = true,
+	-- command settings --
+	runcommand = "!", -- starting of command. ex: !kill
+	divider = " ", -- dividing command and arguments. ex: !kill player
+	viewcommanddocumentation = "?",
+	-- do not mess with below --
+	openbracket = "[",
+	closedbracket = "]"
+}
